62ddb340b76c62a628c5fee23a36eb998e094ffd
Anna <annamaria_ludovica_di_fabio@brown.edu>
2024-12-07 13:35:08 -0500

    added getLocation handler. fixed time

--- server/src/firebase/getLocation.ts
+++ server/src/firebase/getLocation.ts
@@ -2,27 +2,25 @@ import { z } from "zod";
 import { firestore } from "./index.js";
 import { collection, doc, setDoc, getDoc } from "firebase/firestore";
 
-export const addLocationQuerySchema = z
+export const getLocationQuerySchema = z
   .object({
-    lat: z.coerce.number(),
-    long: z.coerce.number(),
     userId: z.string(),
-    time: z.coerce.date(),
   })
 
-
-
-export async function addLocation({
-  userId,
-  ...location
-}: z.infer<typeof addLocationQuerySchema>) {
+export async function getLocation({
+  userId
+}: z.infer<typeof getLocationQuerySchema>) {
   const users = collection(firestore, "users");
 
   const testUserDoc = doc(users, userId);
 
-  await setDoc(testUserDoc, location);
-
   const docRef = await getDoc(testUserDoc);
 
-  return docRef.data();
+  const data = docRef.data();
+
+  if (data &&data.time?.seconds) {
+    data.time = new Date(data.time.seconds * 1000);
+  }
+
+  return data;
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,21 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import "dotenv/config";
-import { addLocation, addLocationQuerySchema } from "./firebase/location.js";
+import { addLocation, addLocationQuerySchema } from "./firebase/addLocation.js";
+import { getLocation, getLocationQuerySchema } from "./firebase/getLocation.js";
 import { zValidator } from "@hono/zod-validator";
 
 const app = new Hono();
 
-app.get("/", zValidator("query", addLocationQuerySchema), async (c) => {
+app.get("/add-location", zValidator("query", addLocationQuerySchema), async (c) => {
   const query = c.req.valid("query");
   const location = await addLocation(query);
+  return c.json(location, 200);
+});
+
+app.get("/get-location", zValidator("query", getLocationQuerySchema), async (c) => {
+  const query = c.req.valid("query");
+  const location = await getLocation(query);
   return c.json(location, 200);
 });
 
