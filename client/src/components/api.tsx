1dc6860e5955279715c0542c6e15d272d8c4ea99
lynnlin77 <lynnlin77@163.com>
2024-12-15 16:15:05 -0500

    add time information to the checkuserlocationspage

--- client/src/components/api.tsx
+++ client/src/components/api.tsx
@@ -1,33 +1,5 @@
 const BASE_URL = "http://localhost:3000";
 
-// Function to add a location
-// export const addLocation = async (
-//   userId: string,
-//   lat: number,
-//   long: number,
-//   time: string
-// ): Promise<{ success: boolean; message?: string }> => {
-//   try {
-//     const response = await fetch(
-//       `${BASE_URL}/add-location?userId=${encodeURIComponent(userId)}&lat=${lat}&long=${long}&time=${encodeURIComponent(
-//         time
-//       )}`,
-//       {
-//         method: "GET",
-//       }
-//     );
-//     if (!response.ok) {
-//       throw new Error(`Error adding location: ${response.statusText}`);
-//     }
-//     return { success: true };
-//   } catch (error: unknown) {
-//     const errorMessage =
-//       error instanceof Error ? error.message : "Unknown error occurred.";
-//     console.error("Error in addLocation:", errorMessage);
-//     return { success: false, message: errorMessage };
-//   }
-// };
-
 export const addLocation = async (
   userId: string,
   lat: number,
@@ -110,8 +82,6 @@ export const addAllowedUserAPI = async (
     return { success: false, message: errorMessage };
   }
 };
-
-
 
 // Function to get allowed users
 export const getAllowedUsers = async (
