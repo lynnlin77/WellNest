56999b1cd85f748d1ae9da6be1a9d1edbe4a1ff0
Anna <annamaria_ludovica_di_fabio@brown.edu>
2024-12-16 15:15:26 -0500

    delete allowed user works

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ import { addLocation, addLocationQuerySchema } from "./firebase/addLocation.js";
 import { getLocation, getLocationQuerySchema } from "./firebase/getLocation.js";
 import { addAllowedUser, addAllowedUserQuerySchema } from "./firebase/addAllowedUser.js";
 import { getAllowedUsers, getAllowedUsersQuerySchema } from "./firebase/getAllowedUsers.js";
+import { deleteAllowedUser, deleteAllowedUserQuerySchema } from "./firebase/deleteAllowedUser.js";
 import { zValidator } from "@hono/zod-validator";
 
 const app = new Hono();
@@ -45,6 +46,12 @@ app.get("/add-allowed-user", zValidator("query", addAllowedUserQuerySchema), asy
 app.get("/get-allowed-users", zValidator("query", getAllowedUsersQuerySchema), async (c) => {
   const query = c.req.valid("query");
   const location = await getAllowedUsers(query);
+  return c.json(location, 200);
+});
+
+app.get("/delete-allowed-user", zValidator("query", deleteAllowedUserQuerySchema), async (c) => {
+  const query = c.req.valid("query");
+  const location = await deleteAllowedUser(query);
   return c.json(location, 200);
 });
 
